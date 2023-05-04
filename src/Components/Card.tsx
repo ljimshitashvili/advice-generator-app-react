371438d7b9a1529959e27cd6a18cc1bfa6806337
ljimshitashvili <jimshitashvili4444@gmail.com>
2023-05-05 01:33:02 +0400

    Finish

--- src/Components/Card.tsx
+++ src/Components/Card.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import styled from "styled-components";
 import axios from "axios";
 import adviceTypes from "../adviceTypes";
